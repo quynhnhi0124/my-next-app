79b4ce72d7bb21bd19e27542446b93bb2819093e
quynhnhi12 <quynhnhi0124@gmail.com>
2024-08-08 07:15:30 +0700

    filter search

--- my-nextjs-app/app/dashboard/items/page.tsx
+++ my-nextjs-app/app/dashboard/items/page.tsx
@@ -12,12 +12,12 @@ interface LevelItem {
 type SortState = {
   id: string;
   desc: boolean;
-}[];
+};
 
 type ItemSearchParams = {
   level?: string[];
   group?: string[];
-  sortBy?: SortState;
+  sortBy?: SortState[];
 }
 
 const itemList: LevelItem[] = [
@@ -64,23 +64,33 @@ const itemList: LevelItem[] = [
 
 ];
 
+type searchState = {
+  page?: number,
+  limit?: number,
+  sort?: { [name: string]: string },
+  filter?: { [name: string]: string }
+}
 //modify url params and call api here
-async function getData(level?: string[], group?: string[], sortBy?: SortState) {
+async function getData(level?: string[], group?: string[], sortBy?: SortState[]) {
   let sort = [];
-  if (typeof sortBy === "string") {
-    const parseData = JSON.parse(sortBy);
-    sort = parseData.length > 0 && parseData.map((item: { id: string, desc: boolean }) => ({
-      [item.id]: item.desc ? "DESC" : "ASC"
-    }))
+  let searchParams: searchState = {};
+  if (!searchParams.sort) {
+    searchParams.sort = {};
   }
+  const parseData = JSON.parse(sortBy);
+  parseData.forEach((item: SortState) => {
+    searchParams.sort![item.id] = item.desc ? "DESC" : "ASC"
+  })
+  console.log(111111111, searchParams);
+
   return itemList;
 }
 
 export default async function ListItem({ searchParams }: { searchParams: ItemSearchParams }) {
   const { level, group, sortBy } = searchParams;
   const data = await getData(level, group, sortBy);
-  const subsidiaryList = [{id: 1, name: "React Sub1"}, {id: 2, name: "Angular Sub2"}];
-  const groupList = [{id: 1, name: "React"}, {id: 2, name: "Angular"}];
+  const subsidiaryList = [{ id: 1, name: "React Sub1" }, { id: 2, name: "Angular Sub2" }];
+  const groupList = [{ id: 1, name: "React" }, { id: 2, name: "Angular" }];
   const tableFilterField = [
     {
       label: "Subsidiary",
